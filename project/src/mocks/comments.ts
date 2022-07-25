b66413730d149759d90c4dd71fd010d658d91adf
iam-vova <iam-vova@yandex.ru>
2022-07-25 11:58:42 +0300

    M4T1 | Добавляет моки комментов

---
+++ project/src/mocks/comments.ts
@@ -0,0 +1,54 @@
+import {Comment} from '../types/comment';
+
+export const Comments: Comment[] = [
+  {
+    comment: 'Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director\'s funniest and most exquisitely designed movies in years.',
+    date: 'Mon Jul 25 2022 11:21:16 GMT+0300 (Moscow Standard Time)',
+    id: 1,
+    rating: 8.9,
+    user: {
+      id: 222,
+      name: 'Kate Muir'
+    }
+  },
+  {
+    comment: 'Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director\'s funniest and most exquisitely designed movies in years.',
+    date: 'Mon Jun 24 2021 7:21:16 GMT+0300 (Moscow Standard Time)',
+    id: 1,
+    rating: 7.9,
+    user: {
+      id: 333,
+      name: 'Кейт Бланшетт'
+    }
+  },
+  {
+    comment: 'Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director\'s funniest and most exquisitely designed movies in years.',
+    date: 'Mon May 23 2020 8:21:16 GMT+0300 (Moscow Standard Time)',
+    id: 1,
+    rating: 6.9,
+    user: {
+      id: 444,
+      name: 'Бен Харди'
+    }
+  },
+  {
+    comment: 'Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director\'s funniest and most exquisitely designed movies in years.',
+    date: 'Mon Apr 22 2019 9:21:16 GMT+0300 (Moscow Standard Time)',
+    id: 1,
+    rating: 5.9,
+    user: {
+      id: 555,
+      name: 'Джейсон Шварцман'
+    }
+  },
+  {
+    comment: 'Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director\'s funniest and most exquisitely designed movies in years.',
+    date: 'Mon Mar 21 2018 10:21:16 GMT+0300 (Moscow Standard Time)',
+    id: 1,
+    rating: 4.9,
+    user: {
+      id: 666,
+      name: 'Джонни Депп'
+    }
+  }
+];
